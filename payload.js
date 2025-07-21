84a7263d47d319960574eca5f75e8b4d786bfb5e
PS4IDEA <76072886+PS4IDEA@users.noreply.github.com>
2025-07-22 02:59:04 +0300

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,10 +1,4 @@
-fetch('./payload1.bin').then(res => {
-    res.arrayBuffer().then(arr => {
-        window.pld = new Uint32Array(arr);
-
-    })
-})
-fetch('./payload2.bin').then(res => {
+fetch('./payload1.bin''./payload2.bin' ).then(res => {
     res.arrayBuffer().then(arr => {
         window.pld = new Uint32Array(arr);
 
