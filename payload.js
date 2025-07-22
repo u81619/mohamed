a54f4b08e58ecf8e1dc3783d9a4ccd4d49186ab7
PS4IDEA <76072886+PS4IDEA@users.noreply.github.com>
2025-07-22 03:05:53 +0300

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,6 +1,16 @@
-fetch('./payload1.bin''./payload2.bin' ).then(res => {
-    res.arrayBuffer().then(arr => {
-        window.pld = new Uint32Array(arr);
+// تحميل payload1.bin
+fetch('./payload1.bin').then(res => {
+    return res.arrayBuffer();
+}).then(arr => {
+    window.pld1 = new Uint32Array(arr);
 
-    })
-})
+    // بعد تحميل payload1.bin، تحميل payload2.bin
+    return fetch('./payload2.bin');
+}).then(res => {
+    return res.arrayBuffer();
+}).then(arr => {
+    window.pld2 = new Uint32Array(arr);
+
+    // هنا يمكنك تنفيذ أي كود لتفعيلهما معًا
+    // مثال: callPayloads(window.pld1, window.pld2);
+});
